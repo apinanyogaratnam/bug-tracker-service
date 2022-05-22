d148b0f3092301c593e04b6b4373f82e7584fde9
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-22 13:11:47 -0400

    fix: specified port for application to run on

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ load_dotenv()
 app = create_app()
 
 debug = os.environ.get('ENVIRONMENT') == 'DEVELOPMENT'
+port = 8000
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(port), debug=debug)
+    app.run(host='0.0.0.0', port=port, debug=debug)
