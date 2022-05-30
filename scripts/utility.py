36d885d6f43dac4cecb0b3d0fc48b4cd67d4ef70
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-29 21:11:41 -0400

    feat: created drop table utililty function

--- scripts/utility.py
+++ scripts/utility.py
@@ -7,13 +7,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def get_postgres_connection():
-    host = os.environ.get('POSTGRES_HOST')
-    database = os.environ.get('POSTGRES_DATABASE')
-    user = os.environ.get('POSTGRES_USER')
-    password = os.environ.get('POSTGRES_PASSWORD')
+def get_postgres_connection() -> psycopg2.connect:
+    host: str = os.environ.get('POSTGRES_HOST')
+    database: str = os.environ.get('POSTGRES_DATABASE')
+    user: str = os.environ.get('POSTGRES_USER')
+    password: str = os.environ.get('POSTGRES_PASSWORD')
 
-    connection = psycopg2.connect(
+    connection: psycopg2.connect = psycopg2.connect(
         host=host,
         database=database,
         user=user,
@@ -21,3 +21,10 @@ def get_postgres_connection():
     )
 
     return connection
+
+
+def drop_table(table_name: str, connection: psycopg2.connect) -> None:
+    with connection.cursor() as cursor:
+        drop_table_query: str = f'DROP TABLE IF EXISTS {table_name}'
+        cursor.execute(drop_table_query)
+        connection.commit()
