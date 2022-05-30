23a020f56aedca65316b5b2df396fa478c1a384f
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-29 22:52:57 -0400

    refactor: using base api class for helper methods for endpoint usage

--- views/__init__.py
+++ views/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 
+from controllers.utility import BaseAPI
 from controllers.root import Root
 from controllers.user import Users
 
@@ -11,8 +12,10 @@ def create_app():
 
     VERSION = 'api/v1'
 
+    resource = {'base_api': BaseAPI}
+
     api.add_resource(Root, '/', f'/{VERSION}/')
 
-    api.add_resource(Users, f'/{VERSION}/users/')
+    api.add_resource(Users, f'/{VERSION}/users/', resource_class_kwargs=resource)
 
     return app
