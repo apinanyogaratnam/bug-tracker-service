f1457d06e7832e48aa867b4fceef034420510d52
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-30 22:39:09 -0400

    feat: user test data loader structure class

---
+++ tests/user_test_data_loader.py
@@ -0,0 +1,32 @@
+from typing import Set
+
+from models.User import User
+
+
+class UserTestDataLoader:
+    def __init__(self: 'UserTestDataLoader') -> None:
+        pass
+
+    def users(self: 'UserTestDataLoader') -> Set[User]:
+        return set([
+            User(
+                internal_user_id=1,
+                external_user_id='external_user_id',
+                username='username',
+                email='email',
+                project_ids=[1, 2, 3],
+                created_at='created_at'
+            ),
+            User(
+                internal_user_id=2,
+                external_user_id='external_user_id_1',
+                username='username1',
+                email='email1',
+                project_ids=[4, 5, 6],
+                created_at='created_at_1'
+            ),
+        ])
+
+    def initialize_database(self: 'UserTestDataLoader') -> None:
+        users: Set[User] = self.users()
+        pass
