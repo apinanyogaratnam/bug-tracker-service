2d2c315626458d8a193e8817dd52ca75714bd10e
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-30 22:41:37 -0400

    fix: renamed class to user dto

--- models/UserDTO.py
+++ models/UserDTO.py
@@ -1,7 +1,7 @@
 from typing import Set
 
 
-class User:
+class UserDTO:
     def __init__(self, internal_user_id: int, external_user_id: str, username: str, email: str, project_ids: Set[int], created_at: str) -> None:
         self.internal_user_id = internal_user_id
         self.external_user_id = external_user_id
