ab7bd37475de2cbf4d844e34aeaf64698e6faa49
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-04 15:40:20 -0400

    feat: created project model

---
+++ models/project.py
@@ -0,0 +1,63 @@
+from typing import Set
+
+from controllers.utility import Utility
+
+
+class Project:
+    def __init__(
+        self: 'Project',
+        project_id: int,
+        user_id: int,
+        administrator_id: int,
+        co_administrator_ids: Set[int],
+        member_ids: Set[int],
+        name: str,
+        description: str,
+        created_at: str
+    ) -> None:
+        self.project_id = project_id
+        self.user_id = user_id
+        self.administrator_id = administrator_id
+        self.co_administrator_ids = co_administrator_ids
+        self.member_ids = member_ids
+        self.name = name,
+        self.description = description
+        self.created_at = created_at
+
+    def create(self: 'Project'):
+        """Creates a new user in the database
+
+        Args:
+            self (User): the user class object
+
+        Raises:
+            Exception: if write query fails
+        """
+        utility_handler = Utility()
+
+        save_user_query: str = '''
+            INSERT INTO projects (
+                project_id,
+                user_id,
+                administrator_id,
+                co_administrator_ids,
+                member_ids,
+                name,
+                description
+            ) VALUES (
+                %s, %s, %s, %s, %s, %s, %s, %s
+            );
+        '''
+
+        records_to_insert = (
+            self.project_id,
+            self.user_id,
+            self.administrator_id,
+            self.co_administrator_ids,
+            self.member_ids,
+            self.name,
+            self.description,
+            self.created_at,
+        )
+
+        utility_handler.write_to_postgres_structured(save_user_query, records_to_insert)
