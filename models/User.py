5274a6480628635d28f55faca3f1a651972ef4df
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-04 15:03:19 -0400

    feat: created create method for users to save into the database

--- models/User.py
+++ models/User.py
@@ -1,5 +1,7 @@
 from typing import Set
 
+from controllers.utility import Utility
+
 
 class User:
     def __init__(self, internal_user_id: int, external_user_id: str, username: str, email: str, project_ids: Set[int], created_at: str) -> None:
@@ -9,3 +11,29 @@ class User:
         self.email = email
         self.project_ids = project_ids
         self.created_at = created_at
+
+    def create(self: 'User'):
+        """Creates a new user in the database
+
+        Args:
+            self (User): the user class object
+
+        Raises:
+            Exception: if write query fails
+        """
+        utility_handler = Utility()
+
+        save_user_query: str = '''
+            INSERT INTO users (
+                external_user_id,
+                username,
+                email,
+                project_ids
+            ) VALUES (
+                %s, %s, %s, %s
+            );
+        '''
+
+        records_to_insert = (self.external_user_id, self.username, self.email, self.project_ids)
+
+        utility_handler.write_to_postgres_structured(save_user_query, records_to_insert)
