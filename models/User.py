cc22a61bccc9235071e87e2850cd36243a9a1186
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-04 15:27:12 -0400

    refactor: adjusting for including internal user id

--- models/User.py
+++ models/User.py
@@ -25,15 +25,16 @@ class User:
 
         save_user_query: str = '''
             INSERT INTO users (
+                internal_user_id,
                 external_user_id,
                 username,
                 email,
                 project_ids
             ) VALUES (
-                %s, %s, %s, %s
+                %s, %s, %s, %s, %s
             );
         '''
 
-        records_to_insert = (self.external_user_id, self.username, self.email, self.project_ids)
+        records_to_insert = (self.internal_user_id, self.external_user_id, self.username, self.email, self.project_ids)
 
         utility_handler.write_to_postgres_structured(save_user_query, records_to_insert)
