fa2bbc775aab8a68d66a4fb4dd017dd70e1d44d6
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-04 16:32:15 -0400

    refactor: added projects test data initializer into test data loader script

--- test_data_loader.py
+++ test_data_loader.py
@@ -1,4 +1,6 @@
 from tests.user_test_data_loader import UserTestDataLoader
+from tests.projects_test_data_loader import ProjectsTestDataLoader
 
 
 UserTestDataLoader().initialize_database()
+ProjectsTestDataLoader().initialize_database()
