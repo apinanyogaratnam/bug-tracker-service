3ade61a6ff25c14dc7e29bce98c9dd140640bb2d
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-04 19:32:58 -0400

    refactor: modified user model to work with test data and production data

--- models/User.py
+++ models/User.py
@@ -6,12 +6,12 @@ from controllers.utility import Utility
 class User:
     def __init__(
         self: 'User',
-        internal_user_id: int,
         external_user_id: str,
         username: str,
         email: str,
-        project_ids: Set[int],
-        created_at: str
+        internal_user_id: int = None,
+        project_ids: Set[int] = None,
+        created_at: str = None,
     ) -> None:
         self.internal_user_id = internal_user_id
         self.external_user_id = external_user_id
@@ -20,7 +20,7 @@ class User:
         self.project_ids = project_ids
         self.created_at = created_at
 
-    def create(self: 'User'):
+    def create(self: 'User', test_mode: bool = False) -> 'User':
         """Creates a new user in the database
 
         Args:
@@ -31,18 +31,33 @@ class User:
         """
         utility_handler = Utility()
 
-        save_user_query: str = '''
-            INSERT INTO users (
-                internal_user_id,
-                external_user_id,
-                username,
-                email,
-                project_ids
-            ) VALUES (
-                %s, %s, %s, %s, %s
-            );
-        '''
+        if test_mode:
+            save_user_query: str = '''
+                INSERT INTO users (
+                    internal_user_id,
+                    external_user_id,
+                    username,
+                    email,
+                    project_ids
+                ) VALUES (
+                    %s, %s, %s, %s, %s
+                );
+            '''
 
-        records_to_insert = (self.internal_user_id, self.external_user_id, self.username, self.email, self.project_ids)
+            records_to_insert = (self.internal_user_id, self.external_user_id, self.username, self.email, self.project_ids)
+        else:
+            save_user_query: str = '''
+                INSERT INTO users (
+                    external_user_id,
+                    username,
+                    email
+                ) VALUES (
+                    %s, %s, %s
+                ) RETURNING internal_user_id;
+            '''
+
+            records_to_insert = (self.external_user_id, self.username, self.email)
 
         utility_handler.write_to_postgres_structured(save_user_query, records_to_insert)
+
+        return self
