204095e3dfe82dd8f578e73ab3c56472b3938f50
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-04 21:19:10 -0400

    feat: created project controller endpoint

---
+++ controllers/project_controller.py
@@ -0,0 +1,61 @@
+from typing import Tuple
+
+from flask import request
+from flask_restful import Resource
+
+from controllers.response import Response
+from controllers.utility import BaseAPI
+from models import Project
+
+
+class ProjectController(Resource, Project):
+    def __init__(self: 'ProjectController', base_api: BaseAPI) -> None:
+        self.base_api = base_api
+
+    def get(self: 'ProjectController', project_id: int = None) -> Response:
+        if project_id is None: return Response(response_data={}, error='project_id cannot be null', status_code=400)
+        project = self.get_project(project_id)
+
+        if project: return Response(response_data=project, status_code=200)
+
+        return Response(response_data={}, error='Not Found', status_code=404)
+
+    def get_project(self: 'ProjectController', project_id: int) -> dict:
+        query_user: str = f'''
+            SELECT
+                project_id,
+                user_id,
+                administrator_id,
+                co_administrator_ids,
+                member_ids,
+                name,
+                description,
+                EXTRACT(EPOCH FROM created_at) AS created_at
+            FROM projects
+            WHERE project_id = '{project_id}';
+        '''
+
+        project: list = self.base_api.create_pandas_table(query_user).to_dict(orient='records')
+
+        return project[0] if project else None
+
+    def post(self: 'ProjectController') -> Response:
+        body: dict | list = request.get_json()
+
+        try:
+            user_id, name, description = self.validate_body(body)
+        except ValueError as error:
+            return Response(response_data={}, error=str(error), status_code=400)
+
+        super().__init__(user_id, administrator_id=user_id, co_administrator_ids=[], member_ids=[], name=name, description=description)
+        project: dict = self.create().jsonify()
+
+        return Response(response_data=project, status_code=201)
+
+    def validate_body(self: 'ProjectController', body: dict) -> Tuple[str, str, str]:
+        user_id, name, description = body.get('user_id'), body.get('name'), body.get('description')
+
+        if not user_id or not name or not description:
+            raise ValueError('Missing required fields in body. Required fields: user_id, name, description')
+
+        return user_id, name, description
