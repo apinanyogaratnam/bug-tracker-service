5fd645b19d061978778e4b5e1943b814b6903a7b
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-12 18:44:21 -0400

    test: created test data loader

---
+++ tests/column_test_data_loader.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from typing import List
+
+from models import Column
+from scripts.create_columns_table import default_raw_column_data
+
+
+class ColumnTestDataLoader:
+    def __init__(self: 'ColumnTestDataLoader') -> None:
+        pass
+
+    def projects(self: 'ColumnTestDataLoader') -> List[Column]:
+        return [
+            Column(
+                column_id=1,
+                project_id=1,
+                raw_column=default_raw_column_data(),
+                created_at=datetime.utcnow(),
+            ),
+            Column(
+                column_id=2,
+                project_id=2,
+                raw_column=default_raw_column_data(),
+                created_at=datetime.utcnow(),
+            ),
+            Column(
+                column_id=3,
+                project_id=3,
+                raw_column=default_raw_column_data(),
+                created_at=datetime.utcnow(),
+            ),
+        ]
+
+    def initialize_database(self: 'ColumnTestDataLoader') -> None:
+        columns: List[Column] = self.projects()
+
+        for column in columns:
+            column.create()
+
+
+if __name__ == '__main__':
+    ColumnTestDataLoader().initialize_database()
