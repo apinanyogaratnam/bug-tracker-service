4bb0e9ec0b9312bab4efa896da83aa4575266e93
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-12 19:01:44 -0400

    refactor: moved test data loader script into if name main idiom condition check

--- test_data_loader.py
+++ test_data_loader.py
@@ -2,6 +2,7 @@ from tests.user_test_data_loader import UserTestDataLoader
 from tests.project_test_data_loader import ProjectTestDataLoader
 from tests.column_test_data_loader import ColumnTestDataLoader
 
-UserTestDataLoader().initialize_database()
-ProjectTestDataLoader().initialize_database()
-ColumnTestDataLoader().initialize_database()
+if __name__ == '__main__':
+    UserTestDataLoader().initialize_database()
+    ProjectTestDataLoader().initialize_database()
+    ColumnTestDataLoader().initialize_database()
