baf47b61065ab268e667320b9f65d5e26b7ce313
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-12 19:01:12 -0400

    test: added column test data loader script to test data loader script

--- test_data_loader.py
+++ test_data_loader.py
@@ -1,6 +1,7 @@
 from tests.user_test_data_loader import UserTestDataLoader
 from tests.project_test_data_loader import ProjectTestDataLoader
-
+from tests.column_test_data_loader import ColumnTestDataLoader
 
 UserTestDataLoader().initialize_database()
 ProjectTestDataLoader().initialize_database()
+ColumnTestDataLoader().initialize_database()
