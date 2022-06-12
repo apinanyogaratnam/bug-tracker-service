32df3ada7aa0c19cfa52583781fca3f017f09ea3
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-12 18:17:16 -0400

    refactor: put create tables script into a name main idiom condition check

--- create_tables.py
+++ create_tables.py
@@ -1,6 +1,6 @@
 from scripts import create_columns_table, create_users_table, create_projects_table
 
-
-create_users_table.main()
-create_projects_table.main()
-create_columns_table.main()
+if __name__ == '__main__':
+    create_users_table.main()
+    create_projects_table.main()
+    create_columns_table.main()
