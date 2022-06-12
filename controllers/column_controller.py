2456e4810de340bef4cfd9a09ee86c3126c4cb66
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-12 19:48:11 -0400

    feat: created column controller to get columns

---
+++ controllers/column_controller.py
@@ -0,0 +1,58 @@
+from flask_restful import Resource
+
+from controllers.response import Response
+from controllers.utility import BaseAPI
+from models import Column
+
+
+class ColumnController(Resource, Column):
+    def __init__(self: 'ColumnController', base_api: BaseAPI) -> None:
+        self.base_api = base_api
+
+    def get(self: 'ColumnController', project_id: int) -> Response:
+        if project_id is None:
+            return Response(response_data={}, error='Missing required query parameter: project_id.', status_code=400)
+
+        columns: list = self.get_columns(project_id)
+
+        if columns is None:
+            return Response(response_data={}, error='Not Found', status_code=404)
+
+        return Response(response_data=columns, status_code=200)
+
+    def get_columns(self: 'ColumnController', project_id: int) -> list | None:
+        query_user: str = f'''
+            SELECT
+                column_id,
+                project_id,
+                raw_columns,
+                EXTRACT(EPOCH FROM created_at) AS created_at
+            FROM columns
+            WHERE project_id = '{project_id}';
+        '''
+
+        columns: list = self.base_api.create_pandas_table(query_user).to_dict(orient='records')
+
+        return columns
+
+    # def post(self: 'ColumnController') -> Response:
+    #     body: dict | list = request.get_json()
+
+    #     try:
+    #         user_id, name, description = self.validate_body(body)
+    #     except ValueError as error:
+    #         return Response(response_data={}, error=str(error), status_code=400)
+
+    #     super().__init__(user_id, administrator_id=user_id, co_administrator_ids=[], member_ids=[], name=name, description=description)
+    #     self.create()
+    #     projects: list = self.get_projects(user_id)
+
+    #     return Response(response_data=projects, status_code=201)
+
+    # def validate_body(self: 'ColumnController', body: dict) -> Tuple[str, str, str]:
+    #     user_id, name, description = body.get('user_id'), body.get('name'), body.get('description')
+
+    #     if not user_id or not name or not description:
+    #         raise ValueError('Missing required fields in body. Required fields: user_id, name, description')
+
+    #     return user_id, name, description
