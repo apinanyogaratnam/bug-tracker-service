efe59e4f2d552142ace08c22973b26d14a9e314d
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-11 21:29:53 -0400

    refactor: returning all projects of user when creating a user endpoint is called

--- controllers/project_controller.py
+++ controllers/project_controller.py
@@ -81,11 +81,12 @@ class ProjectController(Resource, Project):
             return Response(response_data={}, error=str(error), status_code=400)
 
         super().__init__(user_id, administrator_id=user_id, co_administrator_ids=[], member_ids=[], name=name, description=description)
-        project: dict = self.create().jsonify()
+        self.create()
+        projects: list = self.get_projects(user_id)
 
         # TODO: Send email to user
 
-        return Response(response_data=project, status_code=201)
+        return Response(response_data=projects, status_code=201)
 
     def validate_body(self: 'ProjectController', body: dict) -> Tuple[str, str, str]:
         user_id, name, description = body.get('user_id'), body.get('name'), body.get('description')
