d735363ec226a3554b97832529259832d5e191f5
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-12 18:16:34 -0400

    refactor: added create columns table script to create tables script

--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,6 @@
-from scripts import create_users_table, create_projects_table
+from scripts import create_columns_table, create_users_table, create_projects_table
 
 
 create_users_table.main()
 create_projects_table.main()
+create_columns_table.main()
