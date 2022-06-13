f6e05929ce3120f35c61c0bbc811242936b0d9c0
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-12 22:01:57 -0400

    refactor: created post request for creating a new column

--- controllers/column_controller.py
+++ controllers/column_controller.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 from flask_restful import Resource
 from flask import request
@@ -47,45 +47,27 @@ class ColumnController(Resource, Column):
     def jsonify_columns(self: 'ColumnController', columns: List[Column]) -> List[dict]:
         return [column.jsonify() for column in columns]
 
-    # def post(self: 'ColumnController', column_id: int) -> Response:
-    #     body: dict | list = request.get_json()
+    def post(self: 'ColumnController', column_id: int) -> Response:
+        body: dict | list = request.get_json()
 
-    #     column: Column = self.get_column(column_id)
+        try:
+            project_id, raw_columns = self.validate_body(body)
+        except ValueError as error:
+            return Response(response_data={}, error=str(error), status_code=400)
 
-    #     try:
-    #         user_id, name, description = self.validate_body(body)
-    #     except ValueError as error:
-    #         return Response(response_data={}, error=str(error), status_code=400)
+        super().__init__(project_id, raw_columns)
+        self.create()
+        columns: List[Column] = self.get_columns(project_id)
+        serialized_columns: List[dict] = self.jsonify_columns(columns)
 
-    #     super().__init__(user_id, administrator_id=user_id, co_administrator_ids=[], member_ids=[], name=name, description=description)
-    #     self.create()
-    #     projects: list = self.get_projects(user_id)
+        return Response(response_data=serialized_columns, status_code=201)
 
-    #     return Response(response_data=projects, status_code=201)
+    # TODO: create put method
 
-    # def validate_body(self: 'ColumnController', body: dict) -> Tuple[str, str, str]:
-    #     user_id, name, description = body.get('user_id'), body.get('name'), body.get('description')
+    def validate_body(self: 'ColumnController', body: dict) -> Tuple[str, str]:
+        project_id, raw_columns = body.get('project_id'), body.get('raw_columns')
 
-    #     if not user_id or not name or not description:
-    #         raise ValueError('Missing required fields in body. Required fields: user_id, name, description')
+        if not project_id or not raw_columns:
+            raise ValueError('Missing required fields in body. Required fields: project_id, raw_columns.')
 
-    #     return user_id, name, description
-
-    def get_column(self: 'ColumnController', column_id: int) -> Column:
-        query_user: str = f'''
-            SELECT
-                column_id,
-                project_id,
-                raw_columns,
-                EXTRACT(EPOCH FROM created_at) AS created_at
-            FROM columns
-            WHERE column_id = '{column_id}'
-            LIMIT 1;
-        '''
-
-        queried_columns: list = self.base_api.create_pandas_table(query_user).to_dict(orient='records')
-
-        if len(queried_columns) == 0:
-            raise ValueError('Not Found')
-
-        return Column(**queried_columns[0])
+        return project_id, raw_columns
